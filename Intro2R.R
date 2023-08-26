a681c03b5ad5f03f827579607fe953ef6a9413f4
Rafiga Masmaliyeva <47282203+RafigaM@users.noreply.github.com>
2023-08-26 18:17:40 +0200

    Update Intro2R.R

--- Intro2R.R
+++ Intro2R.R
@@ -1,1 +1,14 @@
+# Getting started with R
+# Download and install R (http://cran.r-project.org/) and RStudio (http://www.rstudio.com/) if you do not have them already. 
+
+# install package named "randomForests" from CRAN
+install.packages("randomForests")
+
+##You can install bioconductor packages with a specific installer script.
+
+# get the installer package if you don't have
+install.packages("BiocManager")
+
+# install bioconductor package "rtracklayer"
+BiocManager::install("rtracklayer")
 
